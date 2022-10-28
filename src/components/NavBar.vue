45f9dfafe62f6fa7dcb49d5b86f45d697a5291ce
Renan Lourenco <renanloub@protonmail.com>
2022-10-28 16:55:11 -0300

    lists lesson

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -16,7 +16,16 @@
             </v-toolbar>
 
             <v-navigation-drawer v-model="drawer" app dark class="primary">
-                <p class="error">test</p>
+                <v-list>
+                    <v-list-item v-for="link in links" :key="link.text" router :to="link.route">
+                        <v-list-item-action>
+                            <v-icon class="ma-2">mdi-{{link.icon}}</v-icon>
+                        </v-list-item-action>
+                        <v-list-item-content>
+                            <v-list-item-title class="white--text">{{link.text}}</v-list-item-title>
+                        </v-list-item-content>
+                    </v-list-item>
+                </v-list>
             </v-navigation-drawer>
 
         </nav>
@@ -26,6 +35,11 @@ export default {
     data(){
         return{
             drawer:true,
+            links:[
+                { icon:'view-dashboard', text:'Dashboard', route:'/' },
+                { icon:'folder', text:'My Projects', route:'/projects' },
+                { icon:'account', text:'Team', route:'/team' },
+            ]
         }
     },
 }
