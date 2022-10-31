4138e41273ec8ddf8db6944563017074945e6b90
Renan Lourenco <renanloub@protonmail.com>
2022-10-31 11:28:15 -0300

    created the dashboard carts

--- src/views/DashboardHome.vue
+++ src/views/DashboardHome.vue
@@ -2,8 +2,29 @@
   <div class="dashboard">
     <h1 class="subheading grey--text">Homepage</h1>
     <v-container class="my-5">
-        content
-      </v-container>  
+      
+      <v-card flat class="pa-3">
+        <v-row wrap>
+          <v-col cols="12" sm="12" md="6">
+            <div class="caption grey--text">Project Title</div>
+            <div>Create a new website</div>
+          </v-col>
+          <v-col cols="12" sm="4" md="2">
+            <div class="caption grey--text">Project Title</div>
+            <div>Renan Lourenço</div>
+          </v-col>
+          <v-col cols="12" sm="4" md="2">
+            <div class="caption grey--text">Due by</div>
+            <div>25 de Dezembro 2022</div>
+          </v-col>
+          <v-col cols="12" sm="4" md="2">
+            <div class="caption grey--text">Status</div>
+            <div>Desenvolvendo</div>
+          </v-col>
+        </v-row>
+      </v-card>
+
+    </v-container>  
   </div>
 </template>
 
