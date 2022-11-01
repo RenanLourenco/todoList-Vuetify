2c965ecadb39e4553510439fce86b7b1e637c0cc
Renan Lourenco <renanloub@protonmail.com>
2022-11-01 12:20:10 -0300

    dashboard created

--- src/views/DashboardHome.vue
+++ src/views/DashboardHome.vue
@@ -2,36 +2,106 @@
   <div class="dashboard">
     <h1 class="subheading grey--text">Homepage</h1>
     <v-container class="my-5">
+
       
-      <v-card flat class="pa-3">
-        <v-row wrap>
+      <v-row class="mb-3">
+        <v-tooltip bottom>
+          <template v-slot:activator="{ on, attrs }">
+        <v-btn small plain color="grey" @click="sortBy('titulo')" v-bind="attrs" v-on="on">
+          <v-icon left small>mdi-folder</v-icon>
+          <span class="caption text-lowercase">By Project Name</span>
+        </v-btn>
+        </template>
+        <span>Sort By Title</span>
+        </v-tooltip>
+        <v-tooltip bottom>
+        <template v-slot:activator="{ on, attrs }">
+        <v-btn small plain color="grey" @click="sortBy('responsavel')" v-bind="attrs" v-on="on">
+          <v-icon left small>mdi-human</v-icon>
+          <span class="caption text-lowercase">By Person </span>
+        </v-btn>
+        </template>
+        <span>Sort by Person</span>
+      </v-tooltip>
+      
+      </v-row>
+
+      
+      <v-card v-for="project in projects" :key="project.titulo" flat class="pa-3">
+        <v-row wrap :class="`project ${project.status}`">
           <v-col cols="12" sm="12" md="6">
             <div class="caption grey--text">Project Title</div>
-            <div>Create a new website</div>
+            <div>{{project.titulo}}</div>
           </v-col>
           <v-col cols="12" sm="4" md="2">
             <div class="caption grey--text">Project Title</div>
-            <div>Renan Lourenço</div>
+            <div>{{project.responsavel}}</div>
           </v-col>
           <v-col cols="12" sm="4" md="2">
             <div class="caption grey--text">Due by</div>
-            <div>25 de Dezembro 2022</div>
+            <div>{{project.prazo}}</div>
           </v-col>
           <v-col cols="12" sm="4" md="2">
-            <div class="caption grey--text">Status</div>
-            <div>Desenvolvendo</div>
+            <div>
+              <v-chip :class="`${project.status} white--text caption my-2`">{{project.status}}</v-chip>
+            </div>
           </v-col>
         </v-row>
+        <v-divider></v-divider>
       </v-card>
+      
 
     </v-container>  
   </div>
 </template>
 
 <script>
-
-
   export default {
-
+    data(){
+      return{
+        projects: [ 
+          {titulo:'Design a new website', responsavel:'The net ninja',prazo:'1 de janeiro de 2023', status:'Desenvolvendo'},
+          {titulo:'Code up the homepage', responsavel:'Renan Lourenço',prazo:'2 de janeiro de 2023', status:'Iniciar'},
+          {titulo:'Design video thumbnails', responsavel:'Renan Lourenço',prazo:'3 de janeiro de 2023', status:'Atrasado'},
+          {titulo:'Create a community forum', responsavel:'Renan Lourenço',prazo:'4 de janeiro de 2023', status:'Completo'},
+        ]
+      }
+    },
+    methods:{
+      sortBy(prop){
+        this.projects.sort((a,b)=>a[prop] < b[prop] ? -1 : 1)
+      }
+    }
   }
 </script>
+
+<style>
+  .project.Completo{
+    border-left:4px solid #0ff52d;
+
+  }
+  .project.Desenvolvendo{
+    border-left:4px solid rgb(250, 246, 21);
+    
+  }
+  .project.Iniciar{
+    border-left:4px solid rgb(248, 32, 255);
+    
+  }
+  .project.Atrasado{
+    border-left:4px solid #c70000;
+    
+  }
+  .v-chip.Completo{
+    background: #3cd1c2 !important;
+  }
+  .v-chip.Desenvolvendo{
+    background: #ffaa2c !important;
+  }
+  .v-chip.Atrasado{
+    background:red !important;
+  }
+  .v-chip.Iniciar{
+    background: #0ff52d !important;
+  }
+</style>
--- src/views/TeamPage.vue
+++ src/views/TeamPage.vue
@@ -2,7 +2,7 @@
     <div class="team">
       <h1 class="subheading grey--text">TEAM PAGE</h1>
       <v-container class="my-5">
-        v-content
+        
       </v-container>
     </div>
   </template>
