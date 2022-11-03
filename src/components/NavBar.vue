20659b9fdd2ac7b961417d135396c0e795736b5f
Renan Lourenco <renanloub@protonmail.com>
2022-11-03 09:59:24 -0300

    menu created in navigation bar

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -8,7 +8,38 @@
                     <span>Ninja</span>
                 </v-toolbar-title>
                 <v-spacer></v-spacer>
-                <v-spacer></v-spacer>
+
+                <template>
+                <div class="text-center">
+                    <v-menu offset-y>
+                    <template v-slot:activator="{ on, attrs }">
+                        <v-btn
+                        color="grey"
+                        plain
+                        v-bind="attrs"
+                        v-on="on"
+                        >
+                        <v-icon>mdi-menu</v-icon>
+                        <span>Menu</span>
+                        </v-btn>
+                    </template>
+                    <v-list>
+                        <v-list-item
+                        v-for="link in links"
+                        :key="link.text"
+                        router :to="link.route"
+                        >
+                        <v-list-item-title>{{ link.text }}</v-list-item-title>
+                        </v-list-item>
+                    </v-list>
+                    </v-menu>
+                </div>
+                </template>
+
+
+                <!--Menu-->
+                
+
                 <v-btn plain color="grey">
                     <span>sign out</span>
                     <v-icon right class="hidden-xs-and-down">mdi-logout</v-icon>
