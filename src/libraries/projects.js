d1990f664d6a3796baa1cdd0b920f0b989cc9147
Renan Lourenco <renanloub@protonmail.com>
2022-11-03 09:26:55 -0300

    projects page

---
+++ src/libraries/projects.js
@@ -0,0 +1,14 @@
+const lib = {
+    projects:[ 
+        {titulo:'Design a new website', responsavel:'The net ninja',prazo:'1 de janeiro de 2023', status:'Desenvolvendo'},
+        {titulo:'Code up the homepage', responsavel:'Renan Lourenço',prazo:'2 de janeiro de 2023', status:'Iniciar'},
+        {titulo:'Design video thumbnails', responsavel:'Renan Lourenço',prazo:'3 de janeiro de 2023', status:'Atrasado'},
+        {titulo:'Create a community forum', responsavel:'Renan Lourenço',prazo:'4 de janeiro de 2023', status:'Completo'},
+    ],
+}
+
+
+
+
+
+export default lib
--- src/views/ProjectsPage.vue
+++ src/views/ProjectsPage.vue
@@ -1,8 +1,48 @@
 <template>
   <div class="projects">
-    <h1 class="subheading grey--text">This is an projects page</h1>
+    <h1 class="subheading grey--text">Projects</h1>
     <v-container class="my-5">
-        content
+      <v-expansion-panels>
+        <v-expansion-panel
+        v-for="project in myProjects"
+        :key="project.titulo"
+        >
+            <v-expansion-panel-header>
+              {{project.titulo}}
+            </v-expansion-panel-header>
+            <v-expansion-panel-content>
+              <v-card>
+                <v-card-text class="px-4 grey--text">
+              <div class="font-weight-bold">due by {{project.responsavel}}</div>
+              <div>info</div>
+              </v-card-text>
+            </v-card>
+            </v-expansion-panel-content>
+        </v-expansion-panel>
+      </v-expansion-panels>
       </v-container>
   </div>
 </template>
+
+<script>
+
+import lib from '../libraries/projects'
+
+export default{
+  data(){
+    return{
+      lib
+    }
+  },
+  computed:{
+    myProjects(){
+      let projects = this.lib.projects
+      return projects.filter(project => {
+        return project.responsavel === 'Renan Lourenço'
+      })
+    }
+  }
+}
+
+
+</script>
